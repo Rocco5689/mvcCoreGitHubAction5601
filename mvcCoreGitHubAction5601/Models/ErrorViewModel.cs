62a8b81f35594370fd514d479c84f065b74c0744
Matthew Cavallo <matt.cavallo@outlook.com>
2021-08-31 15:39:17 -0500

    Add project files.

---
+++ mvcCoreGitHubAction5601/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcCoreGitHubAction5601.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
